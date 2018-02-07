7e6fbcc6ded9e3ded516dea6517e3ea1c3f793f0
yyriceberg <15837820975@163.com>
2018-02-07 20:35:37 +0800

    第一次提交

---
+++ src/svnone.java
@@ -0,0 +1,12 @@
+public class svnone {
+    int x=99;
+    public void print(){}
+    public void setX(int x){
+        this.x= x;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
